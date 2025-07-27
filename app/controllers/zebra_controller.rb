0899a8f3ce04a1d979e7604466e24d9bd6253ece
amcgrail3 <111305582+amcgrail3@users.noreply.github.com>
2025-07-27 22:26:15 +0000

    web page working

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -4,6 +4,53 @@ class ZebraController < ApplicationController
   end
 
   def dice_2_6_action
-    render({ :template => "game_templates/dice/2/6"})
+    @rolls = []    # Create a blank array
+
+    2.times do    # 2 times times...
+      die = rand(1..6)    # Generate a random number
+      @rolls.push(die)    # Add the random number to the array 
+    end
+
+    render({ :template => "game_templates/dice_2_6"})
   end
+
+  def dice_2_10_action
+    @rolls = []    # Create a blank array
+
+    2.times do    # 2 times times...
+      die = rand(1..10)    # Generate a random number
+      @rolls.push(die)    # Add the random number to the array 
+    end
+
+    render({ :template => "game_templates/dice_2_10"})
+  end
+
+  def dice_1_20_action
+    @rolls = []    # Create a blank array
+
+    1.times do    # 1 times...
+      die = rand(1..20)    # Generate a random number
+      @rolls.push(die)    # Add the random number to the array 
+    end
+
+    render({ :template => "game_templates/dice_1_20"})
+  end
+  
+  def dice_5_4_action
+    @rolls = []    # Create a blank array
+
+    5.times do    # 2 times times...
+      die = rand(1..4)    # Generate a random number
+      @rolls.push(die)    # Add the random number to the array 
+    end
+
+    render({ :template => "game_templates/dice_5_4"})
+  end
+
 end
+
+  #get("/", { :controller => "zebra", :action => "homepage_action" })
+  #get("/dice/2/6", { :controller => "zebra", :action => "dice_2_6_action" })
+  #get("/dice/2/10", { :controller => "zebra", :action => "dice_2_10_action" })
+  #get("/dice/1/20", { :controller => "zebra", :action => "dice_1_20_action" })
+  #get("/dice/5/4", { :controller => "zebra", :action => "dice_5_4_action" })
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   get("/", { :controller => "zebra", :action => "homepage_action" })
   get("/dice/2/6", { :controller => "zebra", :action => "dice_2_6_action" })
+  get("/dice/2/10", { :controller => "zebra", :action => "dice_2_10_action" })
+  get("/dice/1/20", { :controller => "zebra", :action => "dice_1_20_action" })
+  get("/dice/5/4", { :controller => "zebra", :action => "dice_5_4_action" })
+
 end
-
-
-#<li><a href="/dice/2/6">Roll two 6-sided dice</a></li>
- # <li><a href="/dice/2/10">Roll two 10-sided dice</a></li>
- # <li><a href="/dice/1/20">Roll one 20-sided die</a></li>
- # <li><a href="/dice/5/4">Roll five 4-sided dice</a></li>
